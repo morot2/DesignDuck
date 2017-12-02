609a7d5547e746a85e4a874c3908d30b43810a0c
morot2 <morot3060@gmail.com>
2017-12-02 15:55:17 +0900

    1. image url dynamic mapping, 2. grid design 수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = '다~ 디자인덕이지!';
--- src/app/designs.ts
+++ src/app/designs.ts
@@ -1,7 +1,14 @@
 import {Design} from "./design";
 
 export const DESIGNS : Design[] = [
-  { name : "레오나르도 다빈치", subject : "홍대 디자인과", profileImageUrl : "", designImageUrl:""},
-  { name : "로댕", subject : "건대 건축학과", profileImageUrl : "", designImageUrl:""},
-  { name : "달리 벤 피카소", subject : "연대 조소과", profileImageUrl : "", designImageUrl:""}
+  { name : "레오나르도 다빈치", subject : "홍대 디자인과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "로댕", subject : "건대 건축학과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "달리 벤 피카소", subject : "연대 조소과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "이중섭", subject : "서울대 조소과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "안견", subject : "경기대 철학과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "마네", subject : "연대 조소과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "모네", subject : "연대 조소과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "달리 벤 피카소", subject : "연대 조소과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"},
+  { name : "달리 벤 피카소", subject : "연대 조소과", profileImageUrl : "../../assets/image/man.png", designImageUrl:"../../assets/image/duck.jpg"}
+
 ]
